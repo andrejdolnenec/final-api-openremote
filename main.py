a3fd177873c4364da4f483eabe327a9df2f0e6dc
andrejdolnenec <andrej.dolnenec@gmail.com>
2022-06-17 20:41:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,30 @@ data =  json.load(open('data.json'))
 @app.route('/get/', methods=['GET'])
 def respond():
 
-    price = request.args.get("price", None)
-    squareMeters = request.args.get("squareMeters", None)
+    priceFrom = request.args.get("priceFrom", None)
+    priceTo = request.args.get("priceTo", None)
+    squareMetersFrom = request.args.get("squareMetersFrom", None)
+    squareMetersTo = request.args.get("squareMetersTo", None)
 
     # For debugging
-    print(f"Received: {price}")
-    print(f"Received: {squareMeters}")
+    print(f"Received: {priceFrom}")
+    print(f"Received: {priceTo}")
+    print(f"Received: {squareMetersFrom}")
+    print(f"Received: {squareMetersTo}")
+    
 
     response = {}
 
     # Check if the user sent a name at all
-    if not price:
+    if not priceFrom and not priceTo and not squareMetersFrom and not squareMetersTo:
         response["All Properies"] = " This response will Include All the available realastate properties from the database."
     else:
         response["Selected Properies"] = "This response includes the realastate properties acording to the request argument PRICE and SQUAREMETERES"
+        
+        # the code below is not implemented becouse the concection to the database canot be established. 
+        
+
+
     return jsonify(response)
 
 
